560933c38b8e66204c330afbad726c57f34a9df5
v0lt <mpc-be@mail.ru>
2022-05-21 12:55:30 +0300

    Задаем номер ревизии.

---
+++ Include/Version.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../revision.h"
+
+#ifndef REV_DATE
+#define REV_DATE       0
+#endif
+
+#ifndef REV_HASH
+#define REV_HASH       0
+#endif
+
+#ifndef REV_NUM
+#define REV_NUM        0
+#endif
+
+#define DO_MAKE_STR(x) #x
+#define MAKE_STR(x)    DO_MAKE_STR(x)
+
+#define VER_RELEASE    0
+
+#define VER_MAJOR      0
+#define VER_MINOR      1
+#define VER_BUILD      0
+
+#define VERSION_NUM    VER_MAJOR,VER_MINOR,VER_BUILD,REV_NUM
+#define VERSION_STR    MAKE_STR(VER_MAJOR) "." MAKE_STR(VER_MINOR) "." MAKE_STR(VER_BUILD) "." MAKE_STR(REV_NUM)
---
+++ update_revision.cmd
@@ -0,0 +1,34 @@
+@ECHO OFF
+SETLOCAL
+CD /D %~dp0
+
+ECHO #pragma once > revision.h
+
+SET gitexe="git.exe"
+%gitexe% --version
+IF /I %ERRORLEVEL%==0 GOTO :GitOK
+
+SET gitexe="c:\Program Files\Git\cmd\git.exe"
+IF NOT EXIST %gitexe% set gitexe="c:\Program Files\Git\bin\git.exe"
+IF NOT EXIST %gitexe% GOTO :END
+
+:GitOK
+
+%gitexe% log -1 --date=format:%%Y.%%m.%%d --pretty=format:"#define REV_DATE %%ad%%n" >> revision.h
+%gitexe% log -1 --pretty=format:"#define REV_HASH %%h%%n" >> revision.h
+
+<nul set /p strTemp=#define REV_BRANCH >> revision.h
+%gitexe% symbolic-ref --short HEAD >> revision.h
+IF %ERRORLEVEL% NEQ 0 (
+ECHO LOCAL >> revision.h
+)
+
+<nul set /p strTemp=#define REV_NUM >> revision.h
+%gitexe% rev-list --count HEAD >> revision.h
+IF %ERRORLEVEL% NEQ 0 (
+ECHO 0 >> revision.h
+)
+
+:END
+ENDLOCAL
+EXIT /B
