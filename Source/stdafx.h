0d785b1986fe3d8f284a672deba3dbe7ca46a614
v0lt <mpc-be@mail.ru>
2022-11-11 20:58:19 +0300

    Косметика.

--- Source/stdafx.h
+++ Source/stdafx.h
@@ -5,8 +5,9 @@
 
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN        // Exclude rarely-used stuff from Windows headers
+
 #include <windows.h>
 
 #include "../external/BaseClasses/streams.h"
