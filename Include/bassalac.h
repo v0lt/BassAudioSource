624a5239fc4be8523f7d433e3dc0c0c73ab08224
v0lt <mpc-be@mail.ru>
2024-08-01 17:09:02 +0300

    Обновлен bassalac.dll до версии 2.4.1.

--- Include/bassalac.h
+++ Include/bassalac.h
@@ -1,6 +1,6 @@
 /*
 	BASSALAC 2.4 C/C++ header file
-	Copyright (c) 2016 Un4seen Developments Ltd.
+	Copyright (c) 2016-2024 Un4seen Developments Ltd.
 
 	See the BASSALAC.CHM file for more detailed documentation
 */
@@ -22,9 +22,6 @@ extern "C" {
 #define BASSALACDEF(f) WINAPI f
 #endif
 
-// additional error codes returned by BASS_ErrorGetCode
-#define BASS_ERROR_MP4_NOSTREAM	6000 // non-streamable due to MP4 atom order ("mdat" before "moov")
-
 // BASS_CHANNELINFO type
 #define BASS_CTYPE_STREAM_ALAC	0x10e00
 
@@ -38,12 +35,12 @@ HSTREAM BASSALACDEF(BASS_ALAC_StreamCreateFileUser)(DWORD system, DWORD flags, c
 #if defined(_WIN32) && !defined(NOBASSOVERLOADS)
 static inline HSTREAM BASS_ALAC_StreamCreateFile(BOOL mem, const WCHAR *file, QWORD offset, QWORD length, DWORD flags)
 {
-	return BASS_ALAC_StreamCreateFile(mem, (const void*)file, offset, length, flags|BASS_UNICODE);
+	return BASS_ALAC_StreamCreateFile(mem, (const void*)file, offset, length, flags | BASS_UNICODE);
 }
 
 static inline HSTREAM BASS_ALAC_StreamCreateURL(const WCHAR *url, DWORD offset, DWORD flags, DOWNLOADPROC *proc, void *user)
 {
-	return BASS_ALAC_StreamCreateURL((const char*)url, offset, flags|BASS_UNICODE, proc, user);
+	return BASS_ALAC_StreamCreateURL((const char*)url, offset, flags | BASS_UNICODE, proc, user);
 }
 #endif
 #endif
