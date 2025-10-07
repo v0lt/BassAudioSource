b24cdf3196e336cdb5da4ce4782d54526d57400e
v0lt <mpc-be@mail.ru>
2025-10-07 20:18:20 +0300

    Обновлен bass_aac.dll до версии 2.4.7.

--- Include/bass_aac.h
+++ Include/bass_aac.h
@@ -11,9 +11,6 @@ extern "C" {
 #define BASSAACDEF(f) WINAPI f
 #endif
 
-// additional error codes returned by BASS_ErrorGetCode
-#define BASS_ERROR_MP4_NOSTREAM	6000 // non-streamable due to MP4 atom order ("mdat" before "moov")
-
 // additional BASS_SetConfig options
 #define BASS_CONFIG_MP4_VIDEO	0x10700 // play the audio from MP4 videos
 #define BASS_CONFIG_AAC_MP4		0x10701 // support MP4 in BASS_AAC_StreamCreateXXX functions (no need for BASS_MP4_StreamCreateXXX)
@@ -22,6 +19,9 @@ extern "C" {
 // additional BASS_AAC_StreamCreateFile/etc flags
 #define BASS_AAC_FRAME960		0x1000 // 960 samples per frame
 #define BASS_AAC_STEREO			0x400000 // downmatrix to stereo
+
+// additional BASS_ChannelGetLength/GetPosition/SetPosition mode
+#define BASS_POS_TRACK			7 // track number
 
 // BASS_CHANNELINFO type
 #define BASS_CTYPE_STREAM_AAC	0x10b00 // AAC
